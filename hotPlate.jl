8c33f9014abe001d3dc6ab04bef872f21fc3cfab
tjense25 <tanner.jensen08@gmail.com>
2018-04-09 23:12:06 -0600

    using multiple dispatch for display function

--- hotPlate.jl
+++ hotPlate.jl
@@ -1,30 +1,42 @@
-
 nrow = 1024
 ncol = 1024
 epsilon = 0.1
 its = 0
 
-grid = [(i==1||i==nrow||j==1||j==ncol) ? 0.0 : 50.0 for j in 1:ncol, i in 1:nrow]
-updated_grid = fill(0.0, (nrow,ncol))
+grid = fill(0.0, nrow*ncol)
+for i in 1:nrow-2, j in 2:ncol-1
+	grid[i*ncol + j] = 50.0
+end
+
+updated_grid = fill(0.0, nrow*ncol)
 
 function isStable(grid, updated_grid, nrow, ncol, epsilon)
-	for j in 2:(ncol - 1), i in 2:(nrow - 1)
-		(abs(updated_grid[i,j] - grid[i,j]) > epsilon) && return false
+	for j in 1:ncol-2, i in 2:nrow-1
+		(abs(updated_grid[i*ncol + j] - grid[i*ncol + j]) > epsilon) && return false
 	end
 	return true
 end
 
-function stabilize(grid, updated_grid, nrow, ncol)
-	for j in 2:(ncol - 1), i in 2:(nrow - 1)
-		updated_grid[i,j] = (grid[i - 1, j] + grid[i + 1, j] + grid[i, j - 1] + grid[i, j + 1]) / 4.0
+function display(grid::Array{Float64,1}, nrow, ncol)
+	for i in 0:nrow-1, j in 1:ncol
+		print(grid[i*ncol + j], " ")
+		(j == ncol) && println()
 	end
-	updated_grid
 end
 
 while !isStable(grid, updated_grid, nrow, ncol, epsilon)
-	updated_grid = stabilize(grid, updated_grid, nrow, ncol)
-	grid, updated_grid = updated_grid, grid
+	for i in 1:nrow-2, j in 2:ncol-1
+		updated_grid[i*ncol + j] = (grid[(i - 1)*ncol + j] + 
+					    grid[(i + 1)*ncol + j] + 
+					    grid[i*ncol + j - 1] + 
+					    grid[i*ncol +  j + 1]) / 4
+	end
+	temp = grid
+	grid = updated_grid
+	updated_grid = temp
 	its += 1
+	display(grid, nrow, ncol)
 end
 its -= 1
 println(its)
+
